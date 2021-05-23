4dcf3aa578362d4dd39e5de26e6aac4c1473ee54
LAPTOP-78QD76RI\User <pannawish.hir63@dome.tu.ac.th>
2021-05-23 09:58:28 +0700

    item spin

--- Assets/Script/Collectable.cs
+++ Assets/Script/Collectable.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    
+    [SerializeField] float speedx;
+    [SerializeField] float speedy;
+    [SerializeField] float speedz;
+
+    float x = 0;
+    float y = 0;
+    float z = 0;
     private void OnTriggerEnter(Collider other)
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         player.GetComponent<Player>().increaseBlock();
         Destroy(gameObject);
+    }
+    private void Update()
+    {
+        GetComponent<Transform>().rotation = Quaternion.Euler(x, y, z);
+
+        x = x + speedx;
+        y = y + speedy;
+        z = z + speedz;
     }
 }
